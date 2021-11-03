0197e6b8ad865f115509af3fc0ffeb2dc3002ac8
niabutler <nia.butler@marsden.school.nz>
2021-11-03 15:50:43 +1300

    Backup

---
+++ seeMore.js
@@ -0,0 +1,16 @@
+
+function seeMore(dots, more, button) {
+    var dots = document.getElementById(dots);
+    var moreText = document.getElementById(more);
+    var btnText = document.getElementById(button);
+      
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more"; 
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less"; 
+        moreText.style.display = "inline";
+    }
+}
